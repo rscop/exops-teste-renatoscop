58cb355759266ddc159ab98b9ccbaf23b7c9975b
r_scop <r.scop@Hotmail.com>
2019-10-09 11:43:42 -0300

    webservice inicial

---
+++ webservice.py
@@ -0,0 +1,44 @@
+#!flask/bin/python
+from flask import Flask
+import sys
+import configparser
+import os
+
+app = Flask(__name__)
+
+# Funcao que le arquivo de configurações
+def get_ConfigFile(inifile, section):
+	c = configparser.ConfigParser()
+	dataset = c.read(inifile)
+	if len(dataset) != 1:
+		raise ValueError
+
+	try:
+		c.read(inifile)
+	except Exception:
+		raise e
+
+	# Verifca as keys do arquivo de configuração
+	for key in c[section]:
+		if len(c[section][key]) == 0:
+			fatal("fatal: %s: could not find %s string" % (inifile, key), 1)
+
+	return c[section]
+
+# Metodo para consulta dos dados
+@app.route('/gitHub', methods=['GET'])
+def get():
+
+	# Armazeno a resposta
+	response = '{"status": "Consultado"}'
+
+	return(response)
+
+if __name__ == '__main__':
+
+	config = get_ConfigFile(sys.argv[0]+'.cfg', 'production')
+	ip = config['listen_ip']
+	port = config['listen_port']
+
+	app.run(host=ip, port=port, debug=False)
+	
