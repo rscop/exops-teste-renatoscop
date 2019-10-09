3ac80ac57b967405916adcc51a095eb2cc04ede4
r_scop <r.scop@Hotmail.com>
2019-10-09 19:08:34 -0300

    many fix

--- webservice.py
+++ webservice.py
@@ -41,9 +41,8 @@ def post():
 	content = request.get_json()
 	branche = content["ref"].split('/')[2]
 	commit = content["head_commit"]["message"]
-	if branche == actualBranche:
 
-		status = validatePush(commit)
+	status = validatePush(commit)
 		
 	response = {
 		"branche": branche,
@@ -57,7 +56,6 @@ if __name__ == '__main__':
 	config = get_ConfigFile(sys.argv[0]+'.cfg', 'production')
 	ip = config['listen_ip']
 	port = config['listen_port']
-	actualBranche = config['actualBranche']
 
 	app.run(host=ip, port=port, debug=False)
 	
