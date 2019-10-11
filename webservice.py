589485ce2d4983aeb3daee77c1a74dcaa59b5289
r_scop <r.scop@Hotmail.com>
2019-10-11 14:00:11 -0300

    LogSystem

--- webservice.py
+++ webservice.py
@@ -2,10 +2,36 @@
 from flask import Flask, abort, request, jsonify, g, url_for
 import sys
 import configparser
+import logging
+import logging.handlers
 import os
 from autoDeploy import validatePush, rebaseBranchs
 
 app = Flask(__name__)
+
+
+def logStart():
+	logit(' __________________________________________________')
+	logit('[         INICIANDO SERVICO DE AUTO DEPLOY         ]')
+	logit(' --------------------------------------------------')
+
+# SetupLOG Funnction
+def setuplog(lf,lfBkpCnt,logLevel):
+
+	global gtwlogger
+	gtwlogger = logging.getLogger('GTW_LOG')
+	gtwlogger.setLevel(logging.DEBUG)
+	formatter = logging.Formatter('%(asctime)s-%(message)s')
+	# Add the log message handler to the logger
+	handler = logging.handlers.TimedRotatingFileHandler(lf, when='midnight', interval=1, backupCount=lfBkpCnt)
+	handler.setFormatter(formatter)
+	gtwlogger.addHandler(handler)
+	logStart()
+
+def logit(m):
+	if config['log_mode']=='2':
+		print(m)
+	gtwlogger.debug(m)
 
 # Read configurationFile
 def get_ConfigFile(inifile, section):
@@ -38,30 +64,39 @@ def get_ConfigFile(inifile, section):
 # Method for data query (Verify API Integrity)
 @app.route('/gitHub', methods=['GET'])
 def get():
+	
+	logit('Receiving GET on /gitHub')
 
 	# Response if OK
 	response = '{"status": "Consultado"}'
 
+	logit('GET OK')
 	return(response)
 
 # GH Hook configured for acess this method
 @app.route('/gitHub', methods=['POST'])
 def post():
 
+	logit('Receiving POST on /gitHub')
 	content = request.get_json()
+	logit('Hook from HitHub')
 
 	try:
 		branch = content["ref"].split('/')[2]
 		eventType = "push"
+		logit('Event Push')
+
 	except:
 		branch = content["ref"]
 		eventType = content["ref_type"]
 
 		if eventType == "branch":
+			logit('Event New Branch')
 			rebaseBranchs()
 
 
 	if eventType == "push":
+		logit('Going to Push')
 
 		commit = content["head_commit"]["message"]
 
@@ -76,21 +111,32 @@ def post():
 		}
 		
 	elif eventType == "branch":
+		logit('Going to Branch')
 
 		response = {
 			"branch": branch,
 			"sstatus": "Rebased branchs"
 		}
 	
+	logit('Event OK, sending response to GH')
+	logit(response)
+
 	return(response) 
 
 if __name__ == '__main__':
 
 	config = get_ConfigFile(sys.argv[0]+'.cfg', 'production')
 
+	setuplog(config['logfile'],config['logfile_backup_count'],config['log_level'])
+
+	logit('Log OK')
+	logit('Config File OK')
+	
 	ip = config['listen_ip']
 
 	port = config['listen_port']
 
+	logit('Starting WS')
 	app.run(host=ip, port=port, debug=False)
+	logit('WS finished')
 	
